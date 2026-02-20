f64f4d3e7ed81c9e4b592119838f69cf7da5aeb3
Lex Albrandt <lex.albrandt@gmail.com>
2026-02-20 11:05:55 -0800

    Finish 05 JS

--- 07-javascript/05-destructuring.js
+++ 07-javascript/05-destructuring.js
@@ -1,44 +1,52 @@
 const destructureItems = (input) => {
-  // destructure the name and parents of the character and format the output as shown
+	// destructure the name and parents of the character and format the output as shown
 
-  return input;
+	const { first, last } = input;
+	const { mother, father } = input.allegiance.parents;
+
+	const result = `${first} ${last}
+    daughter of
+    ${mother} and ${father}`;
+	console.log(result);
+
+	return result;
 };
 
 const sansa = {
-  first: 'Sansa',
-  last: 'Stark',
-  allegiance: {
-    house: 'Winterfell',
-    parents: {
-      mother: 'Catelyn Tully',
-      father: 'Eddard Stark',
-    },
-    animals: {
-      direwolf: 'Lady',
-    },
-  },
+	first: "Sansa",
+	last: "Stark",
+	allegiance: {
+		house: "Winterfell",
+		parents: {
+			mother: "Catelyn Tully",
+			father: "Eddard Stark",
+		},
+		animals: {
+			direwolf: "Lady",
+		},
+	},
 };
 
 const daenerys = {
-  first: 'Daenerys',
-  last: 'Targaryen',
-  allegiance: {
-    house: 'Targaryen',
-    parents: {
-      mother: 'Queen Rhaella',
-      father: 'King Aerys II Targaryen',
-    },
-    animals: {
-      dragons: 'Drogon',
-    },
-  },
+	first: "Daenerys",
+	last: "Targaryen",
+	allegiance: {
+		house: "Targaryen",
+		parents: {
+			mother: "Queen Rhaella",
+			father: "King Aerys II Targaryen",
+		},
+		animals: {
+			dragons: "Drogon",
+		},
+	},
 };
 
-console.log(destructureItems(sansa));
+destructureItems(sansa);
 // Sansa Stark
 //   daughter of
 //   Catelyn Tully and Eddard Stark
-console.log(destructureItems(daenerys));
+destructureItems(daenerys);
 // Daenerys Targaryen
 //   daughter of
 //   Queen Rhaella and King Aerys II Targaryen
